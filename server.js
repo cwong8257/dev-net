b4b3262759bf83f84a27b60972ce8a9b4405a65e
Christopher Wong <cwong8257@gmail.com>
2018-04-26 00:19:42 -0400

    Initial commit

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const users = require('./routes/api/users');
+const profile = require('./routes/api/profile');
+const posts = require('./routes/api/posts');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/devconnector');
+
+app.use('/api/users', users);
+app.use('/api/profile', profile);
+app.use('/api/posts', posts);
+
+app.get('/', (req, res) => res.send('Hello'));
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
