05e4a2b0ba48f0d27881a50e39c1ed82ebc86c62
Christopher Wong <cwong8257@gmail.com>
2018-04-26 19:57:06 -0400

    Add create user profile and update user profile routes

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -7,11 +7,6 @@ const User = require('../../models/User');
 
 const router = express.Router();
 
-// @route GET api/profile/test
-// @desc Tests profile route
-// @access Public
-router.get('/test', (req, res) => res.json({ msg: 'profile works' }));
-
 // @route GET api/profile
 // @desc Get current users profile
 // @access Private
@@ -22,10 +17,105 @@ router.get('/', passport.authenticate('jwt', { session: false }), async (req, re
     const profile = await Profile.findOne({ user: req.user.id });
 
     if (!profile) {
-      errors.noProfile = 'There is no profile for this user';
+      errors.profile = 'There is no profile for this user';
       return res.status(404).json(errors);
     }
     return res.json(profile);
+  } catch (err) {
+    return res.status(404).json(err);
+  }
+});
+
+// @route POST api/profile
+// @desc Create user profile
+// @access Private
+router.post('/', passport.authenticate('jwt', { session: false }), async (req, res) => {
+  const errors = {};
+  const profileFields = {};
+  const standardFields = ['handle', 'company', 'location', 'bio', 'status', 'githubUsername'];
+  const socialFields = ['youtube', 'twitter', 'facebook', 'linkedin', 'instagram'];
+
+  profileFields.user = req.user.id;
+
+  standardFields.forEach((field) => {
+    if (req.body[field]) profileFields[field] = req.body[field];
+  });
+
+  profileFields.social = {};
+
+  socialFields.forEach((field) => {
+    if (req.body[field]) profileFields.social[field] = req.body[field];
+  });
+
+  if (typeof req.body.skills !== 'undefined') {
+    profileFields.skills = req.body.skills.split(',').map(word => word.trim());
+  }
+
+  try {
+    const foundProfileByUser = await Profile.findOne({ user: req.user.id });
+
+    if (foundProfileByUser) {
+      errors.profile = 'Profile already exists';
+      return res.status(400).json(errors);
+    }
+    const foundProfileByHandle = await Profile.findOne({ handle: profileFields.handle });
+
+    if (foundProfileByHandle) {
+      errors.handle = 'That handle already exists';
+      return res.status(400).json(errors);
+    }
+    const newProfile = await new Profile(profileFields).save();
+
+    return res.json(newProfile);
+  } catch (err) {
+    return res.status(404).json(err);
+  }
+});
+
+// @route PUT api/profile
+// @desc Update user profile
+// @access Private
+router.put('/', passport.authenticate('jwt', { session: false }), async (req, res) => {
+  const errors = {};
+  const profileFields = {};
+  const standardFields = ['handle', 'company', 'location', 'bio', 'status', 'githubUsername'];
+  const socialFields = ['youtube', 'twitter', 'facebook', 'linkedin', 'instagram'];
+
+  profileFields.user = req.user.id;
+
+  standardFields.forEach((field) => {
+    if (req.body[field]) profileFields[field] = req.body[field];
+  });
+
+  profileFields.social = {};
+
+  socialFields.forEach((field) => {
+    if (req.body[field]) profileFields.social[field] = req.body[field];
+  });
+
+  if (typeof req.body.skills !== 'undefined') {
+    profileFields.skills = req.body.skills.split(',').map(word => word.trim());
+  }
+
+  try {
+    const foundProfileByUser = await Profile.findOne({ user: req.user.id });
+
+    if (!foundProfileByUser) {
+      errors.profile = 'Profile does not exist exists';
+      return res.status(400).json(errors);
+    }
+    const foundProfileByHandle = await Profile.findOne({ handle: profileFields.handle });
+
+    if (foundProfileByHandle) {
+      errors.handle = 'That handle already exists';
+      return res.status(400).json(errors);
+    }
+    const updatedProfile = await Profile.findOneAndUpdate(
+      { user: req.user.id },
+      { $set: profileFields },
+      { new: true },
+    );
+    return res.json(updatedProfile);
   } catch (err) {
     return res.status(404).json(err);
   }
