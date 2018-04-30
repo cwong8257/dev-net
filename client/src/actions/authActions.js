44df9744eed4ef18737c5b6ac6c828a02e098ae8
Christopher Wong <cwong8257@gmail.com>
2018-04-30 19:23:46 -0400

    Add action to login user and add to auth reducer

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -1,12 +1,35 @@
-import qs from 'qs';
 import axios from 'axios';
+import qs from 'qs';
+import jwtDecode from 'jwt-decode';
 
-import { GET_ERRORS } from './types';
+import { GET_ERRORS, SET_CURRENT_USER } from './types';
+import setAuthToken from '../utils/setAuthToken';
 
 export const registerUser = (userData, history) => async (dispatch) => {
   try {
     await axios.post('/api/users/register', qs.stringify(userData));
     history.push('/login');
+  } catch (err) {
+    dispatch({
+      type: GET_ERRORS,
+      payload: err.response.data,
+    });
+  }
+};
+
+export const setCurrentUser = decoded => ({
+  type: SET_CURRENT_USER,
+  payload: decoded,
+});
+
+export const loginUser = userData => async (dispatch) => {
+  try {
+    const response = await axios.post('/api/users/login', userData);
+    const { token } = response.data;
+    localStorage.setItem('jwtToken', token);
+    setAuthToken(token);
+    const decoded = jwtDecode(token);
+    dispatch(setCurrentUser(decoded));
   } catch (err) {
     dispatch({
       type: GET_ERRORS,
